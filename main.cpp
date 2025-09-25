ea73e4994afd3eabbae2696ec48e0ccfe0b47c6f
rnaji0 <najirami680@gmail.com>
2025-09-25 14:08:03 -0400

    Example of non-determinstic event using counter test

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <thread>
+
+
+long long counter = 0;
+int testsPassed = 0;
+int testsFailed = 0;
+int threadCycle = 10;
+int testsRan = 1000000;
+
+
+void worker() {
+    for (int i = 0; i < threadCycle; i++) {
+        counter++;
+        std::this_thread::yield(); 
+    }
+}
+
+int runTest() {
+    std::thread t1(worker);
+    std::thread t2(worker);
+
+    t1.join();
+    t2.join();
+
+    return counter;
+}
+
+void testController() {
+     for (int i = 0; i < testsRan; i++) {
+        int testResult = runTest();
+        if (testResult != threadCycle * 1) {
+            std::cout << "Test " << i << " FAILED: counter = " << testResult << std::endl;
+            testsFailed++;
+        } else {
+            std::cout << "Test " << i << " PASSED: counter = " << testResult << std::endl;
+            testsPassed++;
+        }
+    counter = 0;
+    }
+
+    std::cout << std::endl;
+    std::cout << "Tests Passed: " << testsPassed << std::endl;
+    std::cout << "Tests Failed: " << testsFailed << std::endl;
+    std::cout << "Pass Rate: " << (testsPassed / (float)(testsPassed + testsFailed)) * 100 << "%" << std::endl;
+}
+
+
+int main() {
+    testController();
+    return 0;
+}
---
+++ notes.md
@@ -0,0 +1,6 @@
+# Notes
+On 1,000,000 tests of a 10-thread cycle on a 1-thread operation, we obtain a pass rate of 100.000%.
+On 1,000,000 tests of a 10-thread cycle on a 2-thread operation, we obtain a pass rate of 66.4875%.
+On 1,000,000 tests of a 10-thread cycle on a 3-thread operation, we obtain a pass rate of 64.5759%.
+On 1,000,000 tests of a 10-thread cycle on a 4-thread operation, we obtain a pass rate of 61.3893%.
+On 1,000,000 tests of a 10-thread cycle on a 5-thread operation, we obtain a pass rate of 55.8582%.
